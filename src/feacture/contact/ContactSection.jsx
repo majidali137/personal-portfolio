08cc9d9cc753c5dff902e38337a9b5ad588c0802
Majid Ali <majidwp69.gmail.com>
2023-09-02 17:24:42 +0500

    Complete Contact section

--- src/feacture/contact/ContactSection.jsx
+++ src/feacture/contact/ContactSection.jsx
@@ -1,110 +1,86 @@
+import Heading from "../../ui/Heading";
 export default function ContactSections() {
-
   return (
-    <form className="space-y-8 divide-y divide-gray-200 mx-8 py-9">
-         <div className=" text-center">
-            <h1 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-5xl sm:leading-none lg:text-6xl ">
-            Have an Project in Mind?
-            </h1>
-            
-          </div>
-      <div className="space-y-8 divide-y divide-gray-200">
-        <div className="pt-8">
-          <div className="mt-6 grid grid-cols-1 gap-y-6 gap-x-4 sm:grid-cols-6">
-            <div className="sm:col-span-3">
-              <label
-                htmlFor="first-name"
-                className="block text-sm font-medium text-gray-700"
-              >
-                First name
-              </label>
-              <div className="mt-1">
-                <input
-                  type="text"
-                  name="first-name"
-                  id="first-name"
-                  autoComplete="given-name"
-                  className="block h-8 w-full rounded-all border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm focus:ring-1"
-                />
+    <>
+      <section className="pt-24">
+        <Heading
+          subHeading="Contact With Me"
+          mainHeading="Have a Project in Mind?"
+          description="There are many variations of passages of Lorem Ipsum available but the majority have suffered alteration in some form."
+        />
+      </section>
+      <form className="space-y-8 divide-y sm:px-10 divide-gray-200  pb-24 py-8 px-12   sm:w-[920px] mx-auto">
+        <div className="space-y-8 divide-y divide-gray-200">
+          <div className="">
+            <div className="mt-6 grid grid-cols-1 gap-y-6 gap-x-4 sm:grid-cols-6">
+              <div className="sm:col-span-3">
+                <div className="mt-1">
+                  <input
+                  placeholder="Enter your name"
+                    type="text"
+                    name="first-name"
+                    id="first-name"
+                    autoComplete="given-name"
+                    className="block h-8 w-full focus:border-b-2 focus:border-[#4a6cf7]  focus:ring-0 focus:outline-none  sm:py-8 py-6 px-4 font-semibold  bg-[#f8f8f9]  sm:text-lg text-[#9ca3af]"
+                  />
+                </div>
               </div>
-            </div>
 
-            <div className="sm:col-span-3">
-              <label
-                htmlFor="last-name"
-                className="block text-sm font-medium text-gray-700"
-              >
-                Last name
-              </label>
-              <div className="mt-1">
-                <input
-                  type="text"
-                  name="last-name"
-                  id="last-name"
-                  autoComplete="family-name"
-                  className="block w-full h-8 rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
-                />
-              </div>
-            </div>
-
-            <div className="sm:col-span-4">
-              <label
-                htmlFor="email"
-                className="block text-sm font-medium text-gray-700"
-              >
-                Email address
-              </label>
-              <div className="mt-1">
-                <input
-                  id="email"
-                  name="email"
-                  type="email"
-                  autoComplete="email"
-                  className="block w-full h-8 rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
-                />
-              </div>
-            </div>
-          </div>
-          <div className="overflow-hidden rounded-lg border border-gray-300 shadow-sm focus-within:border-indigo-500 focus-within:ring-1 focus-within:ring-indigo-500">
-            <label htmlFor="title" className="sr-only">
-              Title
-            </label>
-            <input
-              type="text"
-              name="title"
-              id="title"
-              className="block w-full border-0 pt-2.5 text-lg font-medium placeholder-gray-500 focus:ring-0"
-              placeholder="Title"
-            />
-            <label htmlFor="description" className="sr-only">
-              Description
-            </label>
-            <textarea
-              rows={2}
-              name="description"
-              id="description"
-              className="block w-full resize-none border-0 py-0 placeholder-gray-500 focus:ring-0 sm:text-sm"
-              placeholder="Write a description..."
-              defaultValue={""}
-            />
-
-            {/* Spacer element to match the height of the toolbar */}
-            <div aria-hidden="true">
-              <div className="py-2">
-                <div className="h-9" />
-              </div>
-              <div className="h-px" />
-              <div className="py-2">
-                <div className="py-px">
-                  <div className="h-9" />
+              <div className="sm:col-span-3">
+                <div className="mt-1">
+                  <input
+                  placeholder="Company (Optional)"
+                    type="text"
+                    name="last-name"
+                    id="last-name"
+                    autoComplete="family-name"
+                    className="block w-full h-8 sm:py-8 py-6 px-4 font-semibold  bg-[#f8f8f9] border-gray-300 shadow-sm focus:border-b-2 focus:border-[#4a6cf7]  focus:ring-0 focus:outline-none sm:text-lg text-[#9ca3af]"
+                  />
                 </div>
               </div>
             </div>
-          </div>
+            <div className="mt-6 grid grid-cols-1 gap-y-6 gap-x-4 sm:grid-cols-6">
+              <div className="sm:col-span-3">
+                <div className="mt-1">
+                  <input
+                  placeholder="Enter Your email"
+                    type="email"
+                    name="email"
+                    id="email"
+                    autoComplete="given-name"
+                    className="block h-8 sm:py-8 py-6 px-4 font-semibold w-full  bg-[#f8f8f9]  shadow-smfocus:border-b-2 focus:border-b-2 focus:border-[#4a6cf7]  focus:ring-0 focus:outline-none text-[#9ca3af]"
+                  />
+                </div>
+              </div>
 
-      
+              <div className="sm:col-span-3">
+                <div className="mt-1">
+                  <input
+                  placeholder="Phone number"
+                    type="tel"
+                    name="phone-number"
+                    pattern="[0-9]{3}-[0-9]{2}-[0-9]"
+                    id="phone-number"
+                    autoComplete="family-name"
+                    className="block w-full h-8 sm:py-8 py-6 px-4 font-semibold  bg-[#f8f8f9]  shadow-sm focus:border-b-2 focus:border-[#4a6cf7]  focus:ring-0 focus:outline-none sm:text-lg text-[#9ca3af]"
+                  />
+                </div>
+              </div>
+            </div>
+
+            <div className="overflow-hidden  mt-6 shadow-sm focus:border-b-2 focus:border-[#4a6cf7]  focus:ring-0 focus:outline-none">
+              <textarea
+                rows={3}
+                name="description"
+                id="description"
+                className="block w-full sm:py-8 py-6 px-4 font-semibold  bg-[#f8f8f9]  focus:border-b-2 focus:border-[#4a6cf7]  focus:ring-0 focus:outline-none sm:text-lg text-[#9ca3af]"
+                placeholder="Tell me about your project"
+                defaultValue={""}
+              />
+            </div>
+          </div>
         </div>
-      </div>
-    </form>
+      </form>
+    </>
   );
 }
